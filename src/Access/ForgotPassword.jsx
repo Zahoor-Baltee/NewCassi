3e0017f0f1243b847bc12e908c17dc5c5be5aa39
Muhammad Hasnain <hasnain0893@gmail.com>
2024-06-02 03:38:05 +0500

    login update

---
+++ src/Access/ForgotPassword.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/system";
+import Link from '@mui/material/Link';
+
+import logo from '../Assets/Caasi-croped-logo.png'
+
+const Root = styled(Box)({
+    margin: 0,
+    "& .mainContainer": {
+        width: "100%",
+        height: "calc(100vh - 16px)",
+        display: "flex",
+        alignItems: "center",
+        "& .content": {
+            display: "grid",
+            width: "35vw",
+            margin: "auto",
+            padding: "20px",
+            borderRadius: "10px",
+            boxShadow: "0px 2px 12px 2px #cccccc",
+            "& .logo": {
+                display: 'flex',
+                justifyContent: "center",
+                width: "237px"
+            },
+            "& .textField": {
+                marginBottom: "10px",
+                "& .MuiOutlinedInput-notchedOutline": {
+                    border: "none",
+                    backgroundColor: '#EBEBEB',
+                    zIndex: -1
+                }
+            }
+        },
+    },
+});
+const ForgotPassword = () => {
+    const [data, setData] = useState({});
+    const [condition, setCondition] = useState(false);
+
+    const handleChange = (event) => {
+        setData((pre) => ({ ...pre, [event.target.name]: event.target.value }));
+    };
+
+    const handleLogIn = () => {
+        setCondition(true);
+        console.log(data);
+    };
+
+    return (
+        <Root>
+            <Box className="mainContainer">
+                <Box className="content" >
+                    <Box style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <Box className='logo'>
+                            <img src={logo} width='100%' alt="logo" />
+                        </Box>
+                    </Box>
+                    <Box sx={{ textAlign: "start", marginBottom: 3 }}>
+                        <Typography variant="h4">Wellcome!</Typography>
+                    </Box>
+
+                    <Box>
+                        <Typography variant="body1">Email</Typography>
+                        <TextField
+                            className="textField"
+                            required
+                            fullWidth
+                            type="email"
+                            id="email"
+                            size="small"
+                            name="email"
+                            value={data?.email || ""}
+                            onChange={handleChange}
+                            placeholder="Enter Email"
+                            error={condition && !data?.email}
+                            helperText={condition && !data?.email && "Enter Email"}
+                        />
+                    </Box>
+                    <Box>
+                        <Typography variant="body1">Password</Typography>
+                        <TextField
+                            required
+                            fullWidth
+                            className="textField"
+                            size="small"
+                            name="password"
+                            value={data?.password || ""}
+                            onChange={handleChange}
+                            type="password"
+                            id="password"
+                            placeholder="Enter Password"
+                            error={condition && !data?.password}
+                            helperText={condition && !data?.password && "Enter Password"}
+                        />
+                    </Box>
+                    <Link
+                        href="#"
+                        sx={{
+                            textDecoration: 'none',
+                            color: 'primary', // Inherit color from parent
+                            textAlign: 'end',
+                            marginBottom: 2,
+                            // marginTop: 2
+                        }}
+                    >
+                        <Typography variant="body2">Forget Password?</Typography>
+
+                    </Link>
+                    <Button size="large" variant="contained" fullWidth onClick={handleLogIn}>
+                        LogIn
+                    </Button>
+                </Box>
+            </Box>
+        </Root>
+    );
+};
+
+export default ForgotPassword;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Router from './Shell/Router'
+import ForgotPassword from './Access/ForgotPassword';
 
 function App() {
 
   return (
-    <Router />
+    <>
+
+      <Router />
+    </>
   )
 }
 
